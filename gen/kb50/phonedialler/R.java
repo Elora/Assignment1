5c32fb8a700405e4346783ae526b5ad64b6d67ff
dmpeter <dominique@peterseite.com>
2014-11-20 12:22:53 +0100

    Layout

--- gen/kb50/phonedialler/R.java
+++ gen/kb50/phonedialler/R.java
@@ -20,12 +20,29 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int btn_marginLeft=0x7f040002;
+        public static final int btn_marginMiddle=0x7f040003;
+        public static final int btn_marginRight=0x7f040004;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int btn_0=0x7f08000c;
+        public static final int btn_1=0x7f080002;
+        public static final int btn_2=0x7f080003;
+        public static final int btn_3=0x7f080004;
+        public static final int btn_4=0x7f080005;
+        public static final int btn_5=0x7f080006;
+        public static final int btn_6=0x7f080007;
+        public static final int btn_7=0x7f080008;
+        public static final int btn_8=0x7f080009;
+        public static final int btn_9=0x7f08000a;
+        public static final int btn_back=0x7f08000d;
+        public static final int btn_ocd=0x7f08000b;
+        public static final int phone_input=0x7f080000;
+        public static final int show_country=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +53,20 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int btn_0=0x7f050005;
+        public static final int btn_1=0x7f050006;
+        public static final int btn_2=0x7f050007;
+        public static final int btn_3=0x7f050008;
+        public static final int btn_4=0x7f050009;
+        public static final int btn_5=0x7f05000a;
+        public static final int btn_6=0x7f05000b;
+        public static final int btn_7=0x7f05000c;
+        public static final int btn_8=0x7f05000d;
+        public static final int btn_9=0x7f05000e;
+        public static final int btn_back=0x7f05000f;
+        public static final int btn_country=0x7f050004;
+        public static final int btn_ocd=0x7f050010;
+        public static final int hint=0x7f050003;
         public static final int phone_number=0x7f050001;
     }
     public static final class style {

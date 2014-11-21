af577d20e5d7994cb475406020c3ffb3e38c8e13
11021896 <11021896@student.hhs.nl>
2014-11-21 15:32:26 +0100

    Grammar and pictures

--- gen/kb50/phonedialler/R.java
+++ gen/kb50/phonedialler/R.java
@@ -23,7 +23,8 @@ public final class R {
         public static final int text_size=0x7f040002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int dialericon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080013;

8e008cb0bf0ec5a50bf4436eebd7b34f3539a363
0nenote <soufiane.s@gmail.com>
2014-11-21 14:54:12 +0100

    Get/Set country and return data to main activity

--- src/kb50/phonedialler/CountryActivity.java
+++ src/kb50/phonedialler/CountryActivity.java
@@ -1,7 +1,14 @@
 package kb50.phonedialler;
 
+import java.util.Locale;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,22 +16,29 @@ import android.view.View;
 import android.widget.TextView;
 
 public class CountryActivity extends Activity {
-	private TextView country;	
-	private TextView number;	
-	
+	private TextView country;
+	private TextView number;
+	private Intent back;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_country);
+		back = new Intent();
 		
 		number = (TextView) findViewById(R.id.number1);
-		number.setText((getIntent().getStringExtra("tel_country"))+" " + (getIntent().getStringExtra("tel_number")));
-		
+
+		number.setText((getIntent().getStringExtra("tel_country")) + " "
+				+ (getIntent().getStringExtra("tel_number")));
+
 		country = (TextView) findViewById(R.id.country1);
-		String phoneNumber = getCountry(getIntent().getStringExtra("tel_country"));
-		country.setText(phoneNumber);
+		String phoneNumber = number.getText().toString();
+		back.putExtra("backValue", phoneNumber);
+		
+		country.setText(getCountry("+" + phoneNumber));
+
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -43,45 +57,42 @@ public class CountryActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-    private String getCountry(String phoneNumber){
-    	int countrycode = Integer.parseInt(phoneNumber);
-    	String country = "";
-    	switch(countrycode){
-    		case 30:
-    			country = "Greece";
-    		break;
-    		case 31:
-    			country = "Netherlands";
-    		break;
-    		case 32:
-    			country = "Belgium";
-    		break;
-    		case 33:
-    			country = "France";
-    		break;
-    		case 34:
-    			country = "Spain";
-    		break;
-    		case 36:
-    			country = "Hungary";
-    		break;
-    		default:
-    			country = "Something went wrong...";
-    		break;
-    	}
-    	return country;
-    }
-    
-    public void countryOnClick(View v){
-    	switch(v.getId()){
-		case R.id.countryButtonBack:
-			startActivity(new Intent(this, MainActivity.class));
-		break;
-		case R.id.countryButtonCall:
-			//call the number
-		break;
-    	}
-    }
-}
 
+	private String getCountry(String phoneNumber) {
+
+		PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+		String country = "";
+		try {
+
+			PhoneNumber telNummer = phoneUtil.parse(phoneNumber, "");
+			int countryCode = telNummer.getCountryCode();
+			if (!phoneUtil.isValidNumber(telNummer)) {
+				country = "Nummer is niet geldig!";
+			} else {
+				Locale l = new Locale("",
+						phoneUtil.getRegionCodeForNumber(telNummer));
+				country = "Country - " + l.getDisplayCountry()
+						+ System.getProperty("line.separator") + "Code -  +"
+						+ countryCode;
+
+			}
+
+		} catch (NumberParseException e) {
+			country = "Voer een geldig nummer in!";
+		}
+		return country;
+
+	}
+
+	public void countryOnClick(View v) {
+		switch (v.getId()) {
+		case R.id.countryButtonBack:
+			setResult(1,back);
+			finish();
+			break;
+		case R.id.countryButtonCall:
+			// call the number
+			break;
+		}
+	}
+}
--- src/kb50/phonedialler/MainActivity.java
+++ src/kb50/phonedialler/MainActivity.java
@@ -11,147 +11,152 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
 	private EditText phoneFieldCountry;
 	private EditText phoneFieldNumber;
 	private Intent i;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		i = new Intent("kb50.PhoneDialler.CountryActivity");
+		phoneFieldCountry = (EditText) findViewById(R.id.phone_input_country);
+		phoneFieldNumber = (EditText) findViewById(R.id.phone_input_number);
+
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		phoneFieldNumber = (EditText) findViewById(R.id.phone_input_number);
 	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        phoneFieldCountry = (EditText) findViewById(R.id.phone_input_country);
-        phoneFieldNumber = (EditText) findViewById(R.id.phone_input_number);
-        i = new Intent("kb50.PhoneDialler.CountryActivity");
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void onClick(View v){
-    	switch(v.getId()){
-    		case R.id.show_country:
-    			i.putExtra("tel_country", phoneFieldCountry.getText().toString());
-    			i.putExtra("tel_number", phoneFieldNumber.getText().toString());
-    			startActivity(i);
-    			break;
-    		case R.id.btn_1:
-    			place_text("1");
-    			break;
-    		case R.id.btn_2:
-    			place_text("2");
-    			break;
-    		case R.id.btn_3:
-    			place_text("3");
-    			break;
-    		case R.id.btn_4:
-    			place_text("4");
-    			break;
-    		case R.id.btn_5:
-    			place_text("5");
-    			break;
-    		case R.id.btn_6:
-    			place_text("6");
-    			break;
-    		case R.id.btn_7:
-    			place_text("7");
-    			break;
-    		case R.id.btn_8:
-    			place_text("8");
-    			break;
-    		case R.id.btn_9:
-    			place_text("9");
-    			break;
-    		case R.id.btn_0:
-    			place_text("0");
-    			break;
-    		case R.id.btn_back:
-    			remove_text();
-    			break;
-    		case R.id.btn_ocd:
-    			Context context = getApplicationContext();
-    			CharSequence text = "I'm filler...";
-    			int duration = Toast.LENGTH_SHORT;
-    			
-    			Toast toast = Toast.makeText(context, text, duration);
-    			toast.show();
-    			break;    		
-    	}
-    }
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.show_country:
+			String tel = phoneFieldCountry.getText().toString();
+			if (tel.startsWith("00")) {
 
-    private void place_text(String number){
-    	if (phoneFieldCountry.isFocused()){
-    		phoneFieldCountry.setText(phoneFieldCountry.getText() + number);
-    		int position = phoneFieldCountry.length();
-        	Selection.setSelection(phoneFieldCountry.getText(),position);
-    	}
-    	else if (phoneFieldNumber.isFocused()){
-    		phoneFieldNumber.setText(phoneFieldNumber.getText() + number);
-    		int position = phoneFieldNumber.length();
-        	Selection.setSelection(phoneFieldNumber.getText(),position);
-    	}
-    	else{
-    		Context context = getApplicationContext();
-			CharSequence text = "Select a field please";
+				tel = tel.substring(2);
+
+			}
+			i.putExtra("tel_country", tel);
+			i.putExtra("tel_number", phoneFieldNumber.getText().toString());
+			startActivityForResult(i, 1);
+			break;
+		case R.id.btn_1:
+			place_text("1");
+			break;
+		case R.id.btn_2:
+			place_text("2");
+			break;
+		case R.id.btn_3:
+			place_text("3");
+			break;
+		case R.id.btn_4:
+			place_text("4");
+			break;
+		case R.id.btn_5:
+			place_text("5");
+			break;
+		case R.id.btn_6:
+			place_text("6");
+			break;
+		case R.id.btn_7:
+			place_text("7");
+			break;
+		case R.id.btn_8:
+			place_text("8");
+			break;
+		case R.id.btn_9:
+			place_text("9");
+			break;
+		case R.id.btn_0:
+			place_text("0");
+			break;
+		case R.id.btn_back:
+			remove_text();
+			break;
+		case R.id.btn_ocd:
+			Context context = getApplicationContext();
+			CharSequence text = "I'm filler...";
 			int duration = Toast.LENGTH_SHORT;
-			
+
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
-    	}
-    	
-    }
-    
-    private void remove_text(){
-    	if (phoneFieldCountry.isFocused()){
-    		String phrase = phoneFieldCountry.getText().toString();
+			break;
+		}
+	}
+
+	private void place_text(String number) {
+		if (phoneFieldCountry.isFocused()) {
+			phoneFieldCountry.setText(phoneFieldCountry.getText() + number);
+			int position = phoneFieldCountry.length();
+			Selection.setSelection(phoneFieldCountry.getText(), position);
+		} else if (phoneFieldNumber.isFocused()) {
+			phoneFieldNumber.setText(phoneFieldNumber.getText() + number);
+			int position = phoneFieldNumber.length();
+			Selection.setSelection(phoneFieldNumber.getText(), position);
+		} else {
+			Context context = getApplicationContext();
+			CharSequence text = "Select a field please";
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		}
+
+	}
+
+	private void remove_text() {
+		if (phoneFieldCountry.isFocused()) {
+			String phrase = phoneFieldCountry.getText().toString();
 			String rephrase = "";
-			
-			if(phrase.length() >= 1){
+
+			if (phrase.length() >= 1) {
 				rephrase = phrase.substring(0, phrase.length() - 1);
 				phoneFieldCountry.setText(rephrase);
 			}
 			int position = phoneFieldCountry.length();
-        	Selection.setSelection(phoneFieldCountry.getText(),position);
-    	}
-    	else if (phoneFieldNumber.isFocused()){
-    		String phrase = phoneFieldNumber.getText().toString();
+			Selection.setSelection(phoneFieldCountry.getText(), position);
+		} else if (phoneFieldNumber.isFocused()) {
+			String phrase = phoneFieldNumber.getText().toString();
 			String rephrase = "";
-			
-			if(phrase.length() >= 1){
+
+			if (phrase.length() >= 1) {
 				rephrase = phrase.substring(0, phrase.length() - 1);
 				phoneFieldNumber.setText(rephrase);
 			}
 			int position = phoneFieldNumber.length();
-	    	Selection.setSelection(phoneFieldNumber.getText(),position);
-    	}
-    	else{
-    		Context context = getApplicationContext();
+			Selection.setSelection(phoneFieldNumber.getText(), position);
+		} else {
+			Context context = getApplicationContext();
 			CharSequence text = "Select a field please";
 			int duration = Toast.LENGTH_SHORT;
-			
+
 			Toast toast = Toast.makeText(context, text, duration);
 			toast.show();
-    	}
-    	
-    	
-    	
-    }
+		}
+
+	}
 }
